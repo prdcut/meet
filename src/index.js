3cedfa2bda14cde06db2cd4b93078d2ba0510b9e
proudcut <danielfma10@gmail.com>
2021-07-08 12:34:37 +0200

    added atatus setup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('1cc79cf6a4b1424ca3d87e4dfaad56b2').install();
 
 ReactDOM.render(
   <React.StrictMode>
