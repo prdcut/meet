71abd51f7785a56da3286b9929abfdcfd00193a9
proudcut <danielfma10@gmail.com>
2021-09-01 12:01:51 +0200

    update

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -19,8 +19,8 @@ const credentials = {
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  redirect_uris: ["https://proudcut.github.io/meet"],
-  javascript_origins: ["https://proudcut.github.io", "http://localhost:3000"],
+  redirect_uris: ["https://prdcut.github.io/meet"],
+  javascript_origins: ["https://prdcut.github.io", "http://localhost:3000"],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
 const oAuth2Client = new google.auth.OAuth2(
